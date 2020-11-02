7bcfecd46bb1d39a509bbc301b968ade0516ec31
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-11-02 12:24:47 +0530

    Layout completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import product1 from "./assets/product1.jpg";
+import { data } from "./Data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="nav">
+        <div>
+          <h1>HOT</h1>
+        </div>
+        <div>
+          <p>CLOSE</p>
+        </div>
+      </div>
+      <div className="col-6">
+        <img
+          src={product1}
+          className="product-image"
+          alt="product"
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div className="col-6 content">
+        <div className="container">
+          <p>MARCH</p>
+          <div className="row space-around">
+            <div className="col-2">
+              <a className="text-grey">PREVIOUS</a>
+            </div>
+            <div className="col-8">
+              <h1>FABULOUS FACE WASH</h1>
+            </div>
+            <div className="col-2">
+              <a className="text-grey">NEXT</a>
+            </div>
+          </div>
+          <div className="col">
+            <p>Normal to Dry Skin</p>
+            <h4>$39.00</h4>
+          </div>
+          <div className="col mt-3">
+            <p className="text-grey">INGREDIENTS</p>
+            <h6>LAVENDER CLAY, WHITE STEM, CHANDMILE SAND</h6>
+          </div>
+          <div className="row btns">
+            <button>ADD TO CART</button>
+            <button>ADD TO SUBSCRIPTION</button>
+          </div>
+          <div className="col mt-5">
+            <a>SUBSCRIBE</a>
+            <p className="text-grey">FOR EVERY MONTH KIT</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,24 @@
+import product1 from "./assets/product1.jpg";
+import product2 from "./assets/product2.jpg";
+import product3 from "./assets/product1.jpg";
+
+export const data = [
+  {
+    image: product1,
+    name: "Purifying Facial Cream",
+    cost: "$13.60",
+    id: 1,
+  },
+  {
+    image: product2,
+    name: "Fabulous Face oil",
+    cost: "$25.00",
+    id: 2,
+  },
+  {
+    image: product3,
+    name: "Purifying Facial Cream",
+    cost: "$13.60",
+    id: 1,
+  },
+];
